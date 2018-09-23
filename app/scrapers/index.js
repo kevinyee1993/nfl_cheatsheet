f8c753d40b9128472470772f72236a374e5e3d03
Kevin Yee <kevinyee1993@gmail.com>
2018-09-22 19:08:36 -0700

    Finish qb scraper which posts qb name and rank to db

--- app/scrapers/index.js
+++ app/scrapers/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 const axios = require('axios');
-let count = 1;
+let count = 0;
 
 const PASSING_STATS_URL = 'http://www.espn.com/nfl/statistics/player/_/stat/passing/sort/quarterbackRating';
 
@@ -29,23 +29,13 @@ searchStats()
 
 
       if(player !== 'PLAYER') {
+        count++;
         await axios.post('http://localhost:8000/stats', {
           qbName: player,
           qbRank: count
         })
         .then(response => console.log("posted"))
         .catch(error => console.log("error!"));
-
-        // await axios({
-      //     method: 'post',
-      //     url: 'http://localhost:8000/stats',
-      //     data: {
-      //       qbName: player,
-      //       qbRank: count
-      //   }
-      // });
-
-        count++;
       }
 
       /*

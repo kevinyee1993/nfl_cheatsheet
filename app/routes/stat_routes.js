8a4adee677abe715091a539cfb5c47e0e6b885d0
Kevin Yee <kevinyee1993@gmail.com>
2018-09-23 00:35:08 -0700

    Change scraper and backend structure to be more general instead of position focused and add player position as new attribute for each entry

--- app/routes/stat_routes.js
+++ app/routes/stat_routes.js
@@ -1,12 +1,12 @@
 module.exports = function(app, db) {
   // const collection =
 
-  app.get('/qbStats/:qbName', (req, res) => {
+  app.get('/stats/:name', (req, res) => {
 
     // let rank = parseInt(req.params.qbRank);
-    const details = { 'qbName': req.params.qbName };
+    const details = { 'name': req.params.name };
 
-    db.collection('qbStats').findOne(details, (err, item) => {
+    db.collection('stats').findOne(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -15,20 +15,22 @@ module.exports = function(app, db) {
     });
   });
 
-  app.post('/qbStats', (req, res) => {
+  app.post('/stats', (req, res) => {
 
     // need to change this
     // not getting exactly the req i expect
     // problem is the transfer of the object at the axios post
     // call to this point right here, need to figure out where the
     // data went
-    const stat = { qbName: req.body.qbName, qbRank: req.body.qbRank };
+    const stat = { name: req.body.name,
+      rank: req.body.rank,
+      position: req.body.position };
     // const stat = { qbName: "brumbin", qbRank: "frumbin" };
 
     // need to change the note in the parameter
-    console.log(req.body);
+    // console.log(req.body);
 
-    db.collection('qbStats').insert(stat, (err, result) => {
+    db.collection('stats').insert(stat, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
--- app/scrapers/index.js
+++ app/scrapers/index.js
@@ -4,17 +4,20 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 let count = 0;
 
+// can add different urls here for different stats
 const PASSING_STATS_URL = 'http://www.espn.com/nfl/statistics/player/_/stat/passing/sort/quarterbackRating';
 
+
+// pass in different urls here as arg to get the different stats
 function searchStats() {
   return fetch(PASSING_STATS_URL)
     .then(response => response.text());
 }
 
-function postQbStats(qbName, qbRank) {
+function postStats(name, rank) {
   axios.post('http://localhost:8000/stats', {
-    qbName: qbName,
-    qbRank: qbRank
+    name: name,
+    rank: rank
   })
   .then(response => console.log("posted!"))
   .catch(error => console.log("error!"));
@@ -25,31 +28,23 @@ searchStats()
     const $ = cheerio.load(body);
     $('tr td:nth-child(2)').each(async function(i, element) {
       const $element = $(element);
-      let player = $element.text().replace(/,[^,]+$/, "");
+      const text = $element.text();
+
+      let position = text.substr(text.indexOf(",") + 1);
+      console.log(position);
+      let player = text.replace(/,[^,]+$/, "");
 
 
       if(player !== 'PLAYER') {
         count++;
-        await axios.post('http://localhost:8000/qbStats', {
-          qbName: player,
-          qbRank: count
+        await axios.post('http://localhost:8000/stats', {
+          name: player,
+          rank: count,
+          position: position
         })
         .then(response => console.log("posted"))
         .catch(error => console.log("error!"));
       }
-
-      /*
-      axios.post('/user', {
-          firstName: 'Fred',
-          lastName: 'Flintstone'
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      */
     });
 
     count = 1;

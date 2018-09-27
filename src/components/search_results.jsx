42044b45a4c4387e7eed97d53d1ea0d3b5942018
Kevin Yee <kevinyee1993@gmail.com>
2018-09-27 02:33:58 -0700

    Create customized player descriptions based on their ranks

--- src/components/search_results.jsx
+++ src/components/search_results.jsx
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 
 import axios from 'axios';
 
+// update this with all positions
+const POSITIONS = { 'QB': 'quarterback' };
+
 class SearchResults extends Component {
   constructor(props) {
     super(props);
@@ -41,10 +44,42 @@ class SearchResults extends Component {
         </div>
       );
     } else {
+
+      let playerAdj;
+      let statsAdj;
+
+      switch(true) {
+        case(this.state.rank < 10):
+          playerAdj = 'crazy';
+          statsAdj = 'hella';
+          break;
+        case(this.state.rank < 20):
+          console.log("get!");
+          playerAdj = 'solid';
+          statsAdj = 'a good amount of';
+          break;
+        case(this.state.rank < 30):
+          playerAdj = 'decent';
+          statsAdj = 'an ok amount of';
+          break;
+        case(this.state.rank < 40):
+          playerAdj = 'booboo';
+          statsAdj = 'barely any';
+          break;
+        case(this.state.rank < 40):
+          playerAdj = 'trash';
+          statsAdj = 'no';
+          break;
+      }
+
+      let analysis = `${ this.state.name } is a ${ playerAdj } ${ this.state.position }.
+      He will get you ${ statsAdj } ${ this.state.description }.`
+
       return(
         <div>
         <img src={ this.state.image }/>
         <p>Name: { this.state.name }</p>
+        <p>{ analysis }</p>
         <p>Rank: { this.state.rank }</p>
         <p>Position: { this.state.position }</p>
         <p>Description: { this.state.description }</p>
